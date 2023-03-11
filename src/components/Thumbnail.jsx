52253ce7db66029b700ce7e7dfd537628368cd6a
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2023-03-11 17:36:54 +0100

    make everything webp

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,23 +1,16 @@
 import { h, Component } from 'preact';
 
-
 import { Icon } from 'react-icons-kit'
 import { thumbsUp } from 'react-icons-kit/fa/thumbsUp'
 
 export function Thumbnail(props) {
   return (
     <div class="myThumbnail">
-      <a href={`watch.html?id=${props.id}`} title="Redhead poledancing" class="box">
+      <a href={`watch.html?id=${props.id}`} class="box">
         <div class="imgBox">
           <img src={props.img}/>
-          {/* <picture> */}
-            {/* <source type="image/webp" srcset={`/img/thumb/${props.webp}`}/> */}
-            {/* <source srcset={`/img/thumb/${props.img}`}/> */}
-          {/* </picture> */}
-
           <div class="time">{props.time}</div>
           <div class="rating">
-            {/* <ThumbUp/> */}
             <Icon icon={thumbsUp} />
             <span>{props.score}%</span>
           </div>
