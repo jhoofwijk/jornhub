70dc066b28ce4b59aca121d8fa2108cfdb47e7f4
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2023-03-19 15:00:28 +0100

    fix issue with thumbnails stacking wrong

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,7 +1,7 @@
 
 export function Block({title, children}) {
   return (
-    <div class="max-w-7xl m-auto">
+    <div class="max-w-7xl m-auto my-3">
       <div class="text-xl font-semibold">
         {title}
       </div>
