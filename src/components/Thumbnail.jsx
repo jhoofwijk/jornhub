20e20b72bc504d161a073f77270a53bd1e00b708
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2023-03-24 17:54:13 +0100

    make thumbnail smaller on mobile

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -24,8 +24,8 @@ export function Thumbnail(props) {
   return (
     <div class="inline-block w-full sm:w-[233px] box-border overflow-y-hidden">
       <a href={`watch.html?id=${props.id}`} class="block">
-        <div class="w-full relative">
-          <img src={props.img} class="w-full"/>
+        <div class="w-full relative aspect-video overflow-hidden md:aspect-auto">
+          <img src={props.img} class="w-full -mt-3 md:mt-0"/>
           <div class="rounded-md bg-black/70 w-12 right-3 bottom-2 absolute text-center text-sm font-semibold">{props.time}</div>
         </div>
         <div class="px-1 text-right">
